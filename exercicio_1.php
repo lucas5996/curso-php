d099f4e51364ace25f42fc5abee68a5c7865c2d5
lucas5996 <gokumellolucas@gmail.com>
2022-06-21 22:07:07 -0300

    exercicio 1

---
+++ exercicio_1.php
@@ -0,0 +1,13 @@
+<?php
+
+$numero;
+
+    echo "insira um numero";
+    $numero = readline();
+
+    if(($numero % 2) == 0 ){
+    echo 'o numero é par';   
+    }else{
+        echo 'o numero é impar';
+    }
+    ?>
