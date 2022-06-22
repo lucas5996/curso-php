4cc2016bca61efb356d29094f2f3118143426703
lucas5996 <gokumellolucas@gmail.com>
2022-06-21 22:07:27 -0300

    exercicio 2

---
+++ exercicio_2.php
@@ -0,0 +1,14 @@
+<?php
+$n1;
+
+echo 'digite um valor entre 1 e 9:';
+$n1 = readline();
+
+
+
+ if($n1 > 1 and $n1 < 9){
+    echo 'o numero inserido esta dentro da faixa permitida';
+}else {
+    echo 'o numero esta fora da faixa peritida';
+}
+?>
